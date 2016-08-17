fa0235c2b4cfdba0f742b6b71af0865bcfaa5bf4
Danuel Williams <joseph_thesaint@hotmail.com>
2016-08-17 16:12:37 -0500

    User now doesn't have to choose their carrier when processing request| UI Redesign using GridLayout

--- app/src/main/java/com/simplistic/dannyboi/creditrecharge/MainActivity.java
+++ app/src/main/java/com/simplistic/dannyboi/creditrecharge/MainActivity.java
@@ -1,30 +1,37 @@
 package com.simplistic.dannyboi.creditrecharge;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.text.Spanned;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.text.InputFilter;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.jar.Attributes;
 
 
 public class MainActivity extends AppCompatActivity {
 
     static private String NUMBERS_FILE = "numbers";
     private String to_send;
-    private String[] previouslyRechargedNumbers;
     private EditText recipientInput, amountInput;
-    private RadioButton sendToDigicel, sendToLime;
     private InputFilter recipientMask;
+    private TextView serviceProvider;
 
     class eraseEditTexts extends AsyncTask<View, Integer, Boolean>{
 
@@ -56,10 +63,53 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class CreditRequestTask extends AsyncTask<Attributes, Void, Attributes>{
+        @Override
+        protected Attributes doInBackground(Attributes... params){
+            params[0].putValue("carrier" , determineCarrier().toUpperCase());
+
+            return params[0];
+        }
+
+        protected void onPostExecute(Attributes result){
+            if (result.getValue("carrier").equals("DIGICEL")){
+                to_send = buildDigicelRechargeString(result.getValue("recipient"),
+                        result.getValue("amount"));
+                Log.d("sendCreditUSSD", to_send);
+                Intent sendCreditIntent = new Intent(Intent.ACTION_CALL);
+                sendCreditIntent.setData(Uri.parse("tel:" + to_send));
+                if (isCallPermitted()) {
+                    startActivity(sendCreditIntent);
+                }
+                else{
+                    findViewById(R.id.send_credit).setEnabled(false);
+                    showPermissionDeniedToast();
+                }
+
+            }
+            else if (result.getValue("carrier").equals("FLOW") ||
+                    result.getValue("carrier").equals("LIME")){
+                to_send = buildLimeRechargeString(result.getValue("recipient"),
+                        result.getValue("amount"));
+                Log.d("sendCreditSMS", to_send);
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("+124",
+                        null, to_send, null, null);
+            }
+            else {
+                showNoSupportCarrierToast();
+            }
+
+        }
+    }
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serviceProvider = (TextView) findViewById(R.id.credit_vendor);
+        serviceProvider.setText(determineCarrier(this.getBaseContext()));
 
         recipientMask = new InputFilter.LengthFilter(7);
 
@@ -67,46 +117,65 @@ public class MainActivity extends AppCompatActivity {
         recipientInput.setFilters(new InputFilter[]{recipientMask});
         amountInput = (EditText) findViewById(R.id.credit_amount_input);
 
-        sendToDigicel = (RadioButton)findViewById(R.id.vendor_choice1);
-        sendToLime = (RadioButton)findViewById(R.id.vendor_choice2);
-
-        View.OnClickListener sendCreditListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                View parentView = (View) v.getParent();
-                Boolean sendingViaDigicel = sendToDigicel.isChecked();
-                Boolean sendingViaLime = sendToLime.isChecked();
-
-                String recipient = recipientInput.getText().toString().trim();
-                String amount = amountInput.getText().toString().trim();
-
-                if (sendingViaDigicel) {
-                    to_send = buildDigicelRechargeString(recipient, amount);
-                    Log.d("sendCreditUSSD",
-                            to_send);
-                    Intent sendCreditIntent = new Intent(Intent.ACTION_CALL);
-                    sendCreditIntent.setData(Uri.parse("tel:" + to_send));
-                    startActivity(sendCreditIntent);
-                }
-
-                if (sendingViaLime) {
-                    to_send = buildLimeRechargeString(recipient, amount);
-                    Log.d("sendCreditSMS",
-                            to_send);
-                    SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage("124", null, to_send, null, null);
-                }
-
-                new eraseEditTexts().execute(recipientInput, amountInput);
-
-            }
-        };
-
         Button sendCredit = (Button) findViewById(R.id.send_credit);
 
-        sendCredit.setOnClickListener(sendCreditListener);
+        sendCredit.setOnClickListener(new CreditRechargeOnClick());
+    }
+
+    private boolean isCallPermitted(){
+        int callPermissionGranted = PermissionChecker
+                .checkSelfPermission(getApplicationContext(),
+                        Intent.ACTION_CALL);
+
+        return callPermissionGranted == PermissionChecker.PERMISSION_GRANTED;
 
     }
+
+    private void showPermissionDeniedToast(){
+        final Context context = this;
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context,
+                        getResources().getText(R.string.check_permission),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void showNoSupportCarrierToast(){
+        final Context context = this;
+        this.runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                Toast.makeText(context,
+                        getResources().getText(R.string.carrier_no_support),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    class CreditRechargeOnClick implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Attributes parameters = new Attributes();
+
+
+            parameters.putValue("recipient",
+                                ((EditText)findViewById(R.id.credit_recipient_input))
+                                        .getText()
+                                        .toString());
+            parameters.putValue("amount",
+                    ((EditText)findViewById(R.id.credit_amount_input))
+                            .getText()
+                            .toString());
+            new CreditRequestTask().execute(parameters);
+
+            new eraseEditTexts().execute(recipientInput, amountInput);
+        }
+    }
+
+
 
     private String buildDigicelRechargeString(String recipient, String amount){
         StringBuilder creditRechargeUSSD = new StringBuilder();
@@ -127,6 +196,28 @@ public class MainActivity extends AppCompatActivity {
         creditRechargeSMS.append(amount);
 
         return creditRechargeSMS.toString();
+    }
+
+    private String determineCarrier(){
+        Context context = this.getApplicationContext();
+        return determineCarrier(context);
+    }
+
+    private String determineCarrier(Context context){
+        // Determine Carrier and return
+        // the string which represents
+        // that carrier
+
+        String carrierName;
+
+        TelephonyManager telephonyInfo =
+                (TelephonyManager)context
+                        .getSystemService(Context.TELEPHONY_SERVICE);
+
+        carrierName = telephonyInfo.getSimOperatorName();
+        Log.i("CarrierDetermination", carrierName);
+
+        return carrierName;
     }
 
     @Override
@@ -161,5 +252,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
 
